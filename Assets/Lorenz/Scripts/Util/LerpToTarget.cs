693f34ed35c347f1c64fb7476bb6d97b00972525
Lorenz Krautgartner <lorenz.krautgartner@gmail.com>
2016-11-23 14:14:26 +0100

    -updated VRTK -added nose to test if it reduces simulation sickness -added screw prefab -> starting development in new branch

--- Assets/Lorenz/Scripts/Util/LerpToTarget.cs
+++ Assets/Lorenz/Scripts/Util/LerpToTarget.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class LerpToTarget : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public Transform targetTransform;
+    [Range(0, 1)]
+    public float rotationLerpSpeed;
+    [Range(0, 1)]
+    public float translationLerpSpeed;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (targetTransform != null)
+        {
+            if(rotationLerpSpeed < 1)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetTransform.rotation, rotationLerpSpeed);
+            }else
+            {
+                transform.rotation = targetTransform.rotation;
+            }
+            if (translationLerpSpeed < 1)
+            {
+                transform.position = Vector3.Lerp(transform.position, targetTransform.position, translationLerpSpeed);
+            }
+            else
+            {
+                transform.position = targetTransform.transform.position;
+            }
+        }
+
 	}
 }
